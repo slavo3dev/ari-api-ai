c1c2d2885f3d7555655391e87456ebcfc2a6f8fb
SdZika <markozivkovic1982@gmail.com>
2025-11-07 20:36:26 +0100

    remove .env from github

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -2,7 +2,7 @@ import { Module, Global } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createClient } from '@supabase/supabase-js';
 import { SupabaseService } from './supabase.service';
-
+//for test only
 @Global()
 @Module({
   providers: [
