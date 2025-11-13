e759b737b5b1620f1ae7d2aaa82642c01cda86b1
SdZika <markozivkovic1982@gmail.com>
2025-11-13 14:28:25 +0100

    add sources route

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { SupabaseModule } from './supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
 import { PorchModule } from './porch/porch.module';
 import { CommentsModule } from './comments/comments.module';
+import { SourcesModule } from './sources/sources.module';
 
 @Module({
   imports: [
     SupabaseModule,
     PorchModule,
     CommentsModule,
+    SourcesModule,
     ConfigModule.forRoot({
       isGlobal: true, // makes env variables available everywhere
     }),
